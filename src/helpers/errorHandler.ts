424087fceb24219b48963a5fb59db28fce1d2509
Tal Piven <talpivena9@gmail.com>
2021-08-12 14:31:34 +0300

    fixed types

--- src/helpers/errorHandler.ts
+++ src/helpers/errorHandler.ts
@@ -1,11 +1,13 @@
+import { ValidationError } from "express-validator";
+
 interface error extends Error {
   statusCode: number;
-  errors: string[];
+  errors: ValidationError[];
 }
 export const errorHandler = (
   message = "Internal server error",
   status = 500,
-  errors = [] as string[]
+  errors = [] as ValidationError[]
 ) => {
   const error = new Error(message) as error;
   error.statusCode = status;
